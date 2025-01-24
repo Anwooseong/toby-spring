25939141139598599f9688da30b7974f0f86d1a4
Anwooseong <aws5624@naver.com>
2025-01-24 16:31:54 +0900

    섹션3 상속을 통한 확장-환율가져오는 방법이 바뀔수도 있기 때문에 추상메서드로 바꾸고 끼워맞추는 방법 적용

--- src/main/java/tobyspring/tobyspring/PaymentService.java
+++ src/main/java/tobyspring/tobyspring/PaymentService.java
@@ -11,7 +11,7 @@ import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
-public class PaymentService {
+abstract public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
         BigDecimal exRate = getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
@@ -20,22 +20,7 @@ public class PaymentService {
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
 
-    private BigDecimal getExRate(String currency) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = bf.lines().collect(Collectors.joining());
-        bf.close();
+    abstract BigDecimal getExRate(String currency) throws IOException;
 
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = data.rates().get("KRW");
-        return exRate;
-    }
 
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
-    }
 }
---
+++ src/main/java/tobyspring/tobyspring/WepApiExRatePaymentService.java
@@ -0,0 +1,27 @@
+package tobyspring.tobyspring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class WepApiExRatePaymentService extends PaymentService {
+    @Override
+    BigDecimal getExRate(String currency) throws IOException {
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = bf.lines().collect(Collectors.joining());
+        bf.close();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        BigDecimal exRate = data.rates().get("KRW");
+        return exRate;
+    }
+}
