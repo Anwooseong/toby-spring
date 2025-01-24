7ad4c8976bd7fc2b465dcf56fe14eb7554d48d4b
Anwooseong <aws5624@naver.com>
2025-01-24 19:20:46 +0900

    섹션4 테스트와 DI(1) - 테스트대역을 사용하여 외부 서비스의 문제점을 생각하여 사용

--- src/test/java/tobyspring/tobyspring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/tobyspring/payment/PaymentServiceTest.java
@@ -1,37 +1,32 @@
 package tobyspring.tobyspring.payment;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import tobyspring.tobyspring.exrate.WepApiExRateProvider;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class PaymentServiceTest {
 
-    @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
     @Test
-    void prepare() throws IOException {
+    void convertedAmount() throws IOException {
+        testAmount(BigDecimal.valueOf(500), BigDecimal.valueOf(5_000));
+        testAmount(BigDecimal.valueOf(1_000), BigDecimal.valueOf(10_000));
+        testAmount(BigDecimal.valueOf(3_000), BigDecimal.valueOf(30_000));
+
+//        Assertions.assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        Assertions.assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
         // given
-        PaymentService paymentService = new PaymentService(new WepApiExRateProvider());
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
 
         // when
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
-        // then
-        // 환율 정보 가져온다
-        Assertions.assertThat(payment.getExRate()).isNotNull();
-
-        // 원화환산금액 계산
-        Assertions.assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
-
-        // 원화환산금액의 유효시간 계산
-        Assertions.assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        Assertions.assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
     }
 }
