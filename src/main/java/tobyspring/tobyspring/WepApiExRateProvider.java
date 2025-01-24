39db23b7e328db4e7caeb94bccb37449d0c26190
Anwooseong <aws5624@naver.com>
2025-01-24 16:45:49 +0900

    섹션3 클래스의 분리 - 자바에서는 단일 상속밖에 지원을 하지 않기 떄문에 확장해야 될 기능이 많아졌을 경우에 굉장히 복잡해지기때문에 클래스를 분리

--- src/main/java/tobyspring/tobyspring/WepApiExRateProvider.java
+++ src/main/java/tobyspring/tobyspring/WepApiExRateProvider.java
@@ -10,8 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.stream.Collectors;
 
-public class WepApiExRatePaymentService extends PaymentService {
-    @Override
+public class WepApiExRateProvider {
     BigDecimal getExRate(String currency) throws IOException {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -21,7 +20,6 @@ public class WepApiExRatePaymentService extends PaymentService {
 
         ObjectMapper mapper = new ObjectMapper();
         ExRateData data = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = data.rates().get("KRW");
-        return exRate;
+        return data.rates().get("KRW");
     }
 }
