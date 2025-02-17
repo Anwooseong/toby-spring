0742de450d7334e898da520e4bfec822643376dc
Anwooseong <aws5624@naver.com>
2025-02-17 15:11:46 +0900

    섹션5 ApiTemplate 분리 -> 이 다음부터는 이 코드들을 스프링에서 어떤 식으로 사용될지, 어떻게 다듬어야할지, 스프링이 이런 템플릿과 콜백을 기능을 제공하는 것이 있다면 어떤 걸지

--- src/main/java/tobyspring/tobyspring/exrate/WepApiExRateProvider.java
+++ src/main/java/tobyspring/tobyspring/exrate/WepApiExRateProvider.java
@@ -1,54 +1,23 @@
 package tobyspring.tobyspring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import tobyspring.tobyspring.api.ApiExecutor;
-import tobyspring.tobyspring.api.SimpleApiExecutor;
+import tobyspring.tobyspring.api.*;
 import tobyspring.tobyspring.payment.ExRateProvider;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.stream.Collectors;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class WepApiExRateProvider implements ExRateProvider {
 
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
+
         String url = "https://open.er-api.com/v6/latest/" + currency;
-        return runApiForExRate(url, new SimpleApiExecutor());
-    }
 
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return extractExRate(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static BigDecimal extractExRate(String response) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        return data.rates().get("KRW");
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
     }
 
 }
